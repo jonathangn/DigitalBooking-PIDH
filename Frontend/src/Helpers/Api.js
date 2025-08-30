4ca295c9475495908cf4a6b06cc1a72831e35a58
jonathangn <jonathangn@mail.com>
2025-08-30 18:21:15 -0500

    update api

--- Frontend/src/Helpers/Api.js
+++ Frontend/src/Helpers/Api.js
@@ -1,6 +1,6 @@
 // const Api = "http://ec2-13-49-0-184.eu-north-1.compute.amazonaws.com:8080/";
 // const Api = "https://pi-digital-booking.herokuapp.com/"
-const Api = "https://digitalzooking.azurewebsites.net/"
+const Api = "https://digitalbooking-api-grh0gua5dmeya7g5.eastus2-01.azurewebsites.net:8080/"
 // const Api = "http://localhost:8080/";
 
 export default Api;
