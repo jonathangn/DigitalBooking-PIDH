61b6bd4e8d14e19acb117b02eb936b42aa55e130
Jonathan G Nieto <jonathangn@mail.com>
2026-07-20 22:07:06 -0500

    allow domain

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    allowedHosts: 'pf-digitalbooking.indhub.co'
+    allowedHosts: ['pf-digitalbooking.indhub.co']
   },
   base: './',
   build: {
